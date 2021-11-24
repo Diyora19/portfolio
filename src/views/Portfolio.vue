403666f4b18196f5ce441f50aec63c66e1799058
Diyora19 <diyorabonu1999@gmail.com>
2021-11-24 10:23:58 +0500

    portfolioVue changes

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -90,6 +90,9 @@ export default {
                         border-radius: 35px;
                         width: 100%;
                         height: 100%;
+                        object-fit: cover;
+                        position: relative;
+                        
                     }
                 }
                 .card{
