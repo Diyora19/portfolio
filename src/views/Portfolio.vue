cc3af5ba18259ed3408964515b1122c017b7f146
Diyora19 <diyorabonu1999@gmail.com>
2022-01-15 20:58:30 +0500

    added new show button

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -26,6 +26,15 @@
                         </a>
                             
                         </v-btn>
+
+                        <v-btn 
+                            class="btn pa-3"
+                            rounded
+                            text>
+                            <a :href="`https://${ar.show}`" class="grey--text text-decoration-none">
+                            show
+                            </a>
+                        </v-btn>
                         </v-card-actions>
                     </v-card>
                 </v-col>
@@ -47,22 +56,22 @@ export default {
     data() {
         return {
             arr:[
-                {id:1, name: 'filter-table', src: "filter-table.jpg", url: 'filter_table'},
-                {id:2, name: 'filter-cards', src: "filter-cards.jpg", url: 'filter-cards'},
-                {id:3, name: 'google-search', src: "google-search.jpg", url: 'google-search'},
-                {id:4, name: 'image-animation', src: "img-animation.jpg", url: 'img-animation'},
-                {id:5, name: 'image-filter', src: "filter-image.jpg", url: 'img-filter'},
-                {id:6, name: 'calculation', src: "task-2.jpg", url: 'task-2'},
-                {id:7, name: 'img', src: "task-3.jpg", url: 'task-3'},
-                {id:8, name: 'chat', src: "task-4.jpg", url: 'task-4'},
-                {id:9, name: 'clock', src: "clock.jpg", url: 'clock'},
-                {id:10, name: 'football', src: "barsa.jpg", url: 'football'},
-                {id:11, name: 'image-contact', src: "imgcontact.jpg", url: 'img-contact'},
-                {id:12, name: 'jaisone', src: "jaisone.jpg", url: 'jaisone'},
-                {id:13, name: 'calculator', src: "calculator.jpg", url: 'calculator'},
-                {id:14, name: 'spinner', src: "spinner.jpg", url: 'spinner'},
-                {id:15, name: 'website', src: "website.jpg", url: 'website'},
-                {id:16, name: 'wellsoftpro', src: "wellsoftpro.jpg", url: 'wellsoftpro'}
+                {id:1, name: 'filter-table', src: "filter-table.jpg", url: 'filter_table', show: "filtertablee.netlify.app"},
+                {id:2, name: 'filter-cards', src: "filter-cards.jpg", url: 'filter-cards', show: "filterrcard.netlify.app"},
+                {id:3, name: 'google-search', src: "google-search.jpg", url: 'google-search', show: "googlesearches.netlify.app"},
+                {id:4, name: 'image-animation', src: "img-animation.jpg", url: 'img-animation', show: "imganimation.netlify.app"},
+                {id:5, name: 'image-filter', src: "filter-image.jpg", url: 'img-filter', show: "imgfilter.netlify.app"},
+                {id:6, name: 'calculation', src: "task-2.jpg", url: 'task-2', show: "simplecalculationpage.netlify.app"},
+                {id:7, name: 'img', src: "task-3.jpg", url: 'task-3', show: "selectimg.netlify.app"},
+                {id:8, name: 'chat', src: "task-4.jpg", url: 'task-4', show: "makesimplechat.netlify.app"},
+                {id:9, name: 'clock', src: "clock.jpg", url: 'clock', show: "coloredclock.netlify.app"},
+                {id:10, name: 'football', src: "barsa.jpg", url: 'football', show: "simplefootball.netlify.app"},
+                {id:11, name: 'image-contact', src: "imgcontact.jpg", url: 'img-contact', show: "imgcontact.netlify.app"},
+                {id:12, name: 'jaisone', src: "jaisone.jpg", url: 'jaisone', show: "jaisone.netlify.app"},
+                {id:13, name: 'calculator', src: "calculator.jpg", url: 'calculator', show: "simplegreencalculator.netlify.app"},
+                {id:14, name: 'spinner', src: "spinner.jpg", url: 'spinner', show: "simplespinner.netlify.app"},
+                {id:15, name: 'website', src: "website.jpg", url: 'website', show: "simpleewebsite.netlify.app"},
+                {id:16, name: 'wellsoftpro', src: "wellsoftpro.jpg", url: 'wellsoftpro', show: "wellsoftpro.netlify.app"}
             ],
 
             
